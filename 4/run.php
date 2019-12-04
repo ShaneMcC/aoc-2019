e309e7a2c66508c564a577f8534d8e824b6d27ff
Shane Mc Cormack <shanemcc@gmail.com>
2019-12-04 12:10:53 +0000

    Only loop once for 50% speed improvement.

--- 4/run.php
+++ 4/run.php
@@ -4,42 +4,47 @@
 	$input = getInputLine();
 	[$start, $end] = explode('-', $input, 2);
 
-	function vaildPassword($password, $part2 = false) {
-		$hasDouble = false;
-		$skip = false;
+	function vaildPassword($password) {
+		$validPart1 = false;
+		$validPart2 = false;
+
+		$skipAhead = false;
 		if (!preg_match('#[0-9]{6}#', $password)) { return false; }
 
 		$pw = str_split($password);
 		for ($i = 0; $i < count($pw); $i++) {
+			 // Ignore first becuase we can't look behind.
 			if ($i == 0) { continue; }
-			if ($skip && $pw[$i] == $pw[$i - 1]) { continue; } else { $skip = false; }
-
+			// Skip ahead if needed
+			if ($skipAhead && $pw[$i] == $pw[$i - 1]) { continue; } else { $skipAhead = false; }
+			// If we're lower than the previous, fail.
 			if ($pw[$i] < $pw[$i - 1]) { return false; }
 
+			// Are we the same as previous?
 			if ($pw[$i] == $pw[$i - 1]) {
-				if ($part2) {
-					if (isset($pw[$i + 1]) && $pw[$i + 1] == $pw[$i]) {
-						// This doesn't count as a double, so skip ahead past
-						// any of the same number.
-						$skip = true;
-					} else {
-						$hasDouble = true;
-					}
+				$validPart1 = true;
+				if (isset($pw[$i + 1]) && $pw[$i + 1] == $pw[$i]) {
+					// This doesn't count as a double for part 2 becuase there
+					// are more than 2 in a row of the same number, so just
+					// skip ahead past any of the same number and only count
+					// as valid for part 1.
+					$skipAhead = true;
 				} else {
-					$hasDouble = true;
+					$validPart2 = true;
 				}
 			}
 		}
 
-		return $hasDouble;
+		return [$validPart1, $validPart2];
 	}
 
 	$part1 = 0;
 	$part2 = 0;
 
 	for ($i = $start; $i <= $end; $i++) {
-		if (vaildPassword($i)) { $part1++; }
-		if (vaildPassword($i, true)) { $part2++; }
+		[$validPart1, $validPart2] = vaildPassword($i);
+		if ($validPart1) { $part1++; }
+		if ($validPart2) { $part2++; }
 	}
 
 	echo 'Part 1: ', $part1, "\n";
