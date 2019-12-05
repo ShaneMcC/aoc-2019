2a236620abd48c0d4db32e4b3f6248f7e7b9f138
Shane Mc Cormack <shanemcc@gmail.com>
2019-12-05 12:22:18 +0000

    Day 5. Part 1. 7714

--- common/IntCodeVM.php
+++ common/IntCodeVM.php
@@ -18,10 +18,15 @@
 			 *
 			 * @param $vm VM to execute on.
 			 * @param $args Args for this instruction.
+			 * @param $modes Parameter modes.
 			 */
-			$this->instrs['1'] = ['ADD', 3, function($vm, $args) {
+			$this->instrs['1'] = ['ADD', 3, function($vm, $args, $modes = []) {
 				[$x, $y, $z] = $args;
-				$vm->setData($z, ($vm->getData($x) + $vm->getData($y)));
+
+				$xMode = isset($modes[0]) ? $modes[0] : 0;
+				$yMode = isset($modes[1]) ? $modes[1] : 0;
+
+				$vm->setData($z, ($vm->getData($x, $xMode) + $vm->getData($y, $yMode)));
 			}];
 
 			/**
@@ -32,10 +37,48 @@
 			 *
 			 * @param $vm VM to execute on.
 			 * @param $args Args for this instruction.
+			 * @param $modes Parameter modes.
 			 */
-			$this->instrs['2'] = ['MUL', 3, function($vm, $args) {
+			$this->instrs['2'] = ['MUL', 3, function($vm, $args, $modes = []) {
 				[$x, $y, $z] = $args;
-				$vm->setData($z, ($vm->getData($x) * $vm->getData($y)));
+
+				$xMode = isset($modes[0]) ? $modes[0] : 0;
+				$yMode = isset($modes[1]) ? $modes[1] : 0;
+
+				$vm->setData($z, ($vm->getData($x, $xMode) * $vm->getData($y, $yMode)));
+			}];
+
+			/**
+			 * INPUT
+			 *   - 3 Z
+			 *
+			 * sets memory position Z to the value from the input.
+			 *
+			 * @param $vm VM to execute on.
+			 * @param $args Args for this instruction.
+			 * @param $modes Parameter modes.
+			 */
+			$this->instrs['3'] = ['INPUT', 1, function($vm, $args, $modes = []) {
+				[$z] = $args;
+				$vm->setData($z, $vm->getInput());
+			}];
+
+			/**
+			 * OUTPUT
+			 *   - 4 Z
+			 *
+			 * Outputs memory position Z
+			 *
+			 * @param $vm VM to execute on.
+			 * @param $args Args for this instruction.
+			 * @param $modes Parameter modes.
+			 */
+			$this->instrs['4'] = ['OUTPUT', 1, function($vm, $args, $modes = []) {
+				[$z] = $args;
+
+				$zMode = isset($modes[0]) ? $modes[0] : 0;
+
+				$vm->appendOutput($vm->getData($z, $zMode));
 			}];
 
 			/**
@@ -47,10 +90,53 @@
 			 * @param $vm VM to execute on.
 			 * @param $args Args for this instruction.
 			 */
-			$this->instrs['99'] = ['HALT', 0, function($vm, $args) {
+			$this->instrs['99'] = ['HALT', 0, function($vm, $args, $modes = []) {
 				$vm->jump(count($this->data)); // Jump to end to halt.
 			}];
 		}
+
+		// Turn output into a queue.
+		public function clearOutput() { $this->output = []; }
+		public function getOutputLength() { return count($this->output); }
+		public function appendOutput($str) { $this->output[] = $str; }
+		public function setOutput($str) { $this->output[] = [$str]; }
+		public function getOutput() { return array_shift($this->output); }
+		public function getAllOutput() { return $this->output; }
+
+		/** Input queue for the VM. */
+		protected $input = '';
+
+		public function clearInput() { $this->input = []; }
+		public function getInputLength() { return count($this->input); }
+		public function appendInput($str) { $this->input[] = $str; }
+		public function setInput($str) { $this->input[] = [$str]; }
+		public function getInput() { return array_shift($this->input); }
+		public function getAllInput() { return $this->input; }
+
+		function reset() {
+			parent::reset();
+			$this->clearInput();
+		}
+
+
+		/**
+		 * Get the data at the given location, understanding mode parameters.
+		 *
+		 * @param $location Data location (or NULL for current).
+		 * @param $mode Mode, 0 for position, 1 for immediate.
+		 * @return Data from location.
+		 */
+		public function getData($loc = null, $mode = 0) {
+			if ($loc === null) { $loc = $this->getLocation(); }
+			if ($mode == 0) {
+				if (isset($this->data[$loc])) { return $this->data[$loc]; }
+			} else if ($mode == 1) {
+				return $loc;
+			}
+
+			throw new Exception('Unknown Data Location: ' . $loc);
+		}
+
 
 		/**
 		 * Step a single instruction.
@@ -60,17 +146,33 @@
 		 */
 		function doStep() {
 			$next = $this->data[$this->location];
-			[$name, $argCount, $ins] = $this->getInstr($next);
+			[$instr, $modes] = $this->getParameterAndModes($next);
+
+			[$name, $argCount, $ins] = $this->getInstr($instr);
 
 			$args = array_slice($this->data, ($this->location + 1), $argCount);
 			$this->location += $argCount;
 
 			if ($this->debug) {
-				echo sprintf('(%4s)   %-20s', $this->location, static::instrToString([$name . '{' . $next . '}', $args])), "\n";
+				echo sprintf('(%4s)   %-20s', $this->location, static::instrToString([$name . '{' . $next . '=>' . $instr . '/'. implode(',', $modes) . '}', $args])), "\n";
 				usleep($this->sleep);
 			}
 
-			$ins($this, $args);
+			$ins($this, $args, $modes);
+		}
+
+		function getParameterAndModes($code) {
+			// TODO: There will be nicer mathsy ways of doing this that we'll
+			//       figure out later.
+
+			$bits = str_split($code);
+
+			$mode = array_pop($bits);
+			$mode = (int)(array_pop($bits) . $mode);
+
+			$bits = array_reverse($bits);
+
+			return [$mode, $bits];
 		}
 
 		/**
