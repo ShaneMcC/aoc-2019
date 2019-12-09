2f435c5b178426ede06e131089011fc64d5ef218
Shane Mc Cormack <shanemcc@gmail.com>
2019-12-09 11:56:38 +0000

    Day 9. Part 2. 4971

--- 9/run.php
+++ 9/run.php
@@ -4,13 +4,17 @@
 	require_once(dirname(__FILE__) . '/../common/IntCodeVM.php');
 	$input = getInputLine();
 
+	function runBOOST($input, $value) {
+		$vm = new IntCodeVM(IntCodeVM::parseInstrLines($input));
+		$vm->setDebug(isDebug());
+		$vm->appendInput($value);
+		$vm->run();
+		$output = $vm->getAllOutput();
+		return array_pop($output);
+	}
 
-	$vm = new IntCodeVM(IntCodeVM::parseInstrLines($input));
-	$vm->appendInput(1);
-	$vm->setDebug(isDebug());
-	$vm->run();
-
-	$output = $vm->getAllOutput();
-	$part1 = array_pop($output);
-
+	$part1 = runBOOST($input, 1);
 	echo 'Part 1: ', $part1, "\n";
+
+	$part2 = runBOOST($input, 2);
+	echo 'Part 2: ', $part2, "\n";
