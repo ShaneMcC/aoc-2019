430407290f643f3d3c3734e06d2efe0ff4ce02f9
Shane Mc Cormack <shanemcc@gmail.com>
2019-12-11 12:43:35 +0000

    Commonify text decoding.

--- 11/run.php
+++ 11/run.php
@@ -2,6 +2,7 @@
 <?php
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	require_once(dirname(__FILE__) . '/../common/IntCodeVM.php');
+	require_once(dirname(__FILE__) . '/../common/decodeText.php');
 	$input = getInputLine();
 
 	$directions = ['^' => ['rotations' => ['<', '>'], 'movement' => ['x' => 0, 'y' => -1]],
@@ -81,88 +82,31 @@
 		return [$map];
 	}
 
-	function drawMap($painted) {
+	function flattenMap($painted, $white = '█', $black = ' ', $offsetY = 0, $offsetX = 0) {
 		[$minX, $minY, $maxX, $maxY] = getBoundingBox($painted);
 
 		$map = [];
-		foreach (yieldXY($minX, $minY, $maxX, $maxY, true) as $x => $y) {
-			$map[$y][$x] = ' ';
+		foreach (yieldXY($minX + $offsetX, $minY + $offsetY, $maxX, $maxY, true) as $x => $y) {
+			$map[$y][$x] = $black;
 			if (isset($painted[$y][$x])) {
-				$map[$y][$x] = $painted[$y][$x] == '#' ? '█' : ' ';
+				$map[$y][$x] = $painted[$y][$x] == '#' ? $white : $black;
 			}
 		}
 
-		foreach ($map as $row) { echo implode('', $row), "\n"; }
+		return $map;
 	}
 
-	$encodedChars = ['011001001010010111101001010010' => 'A',
-	                 '111001001011100100101001011100' => 'B',
-	                 '011001001010000100001001001100' => 'C',
-	                 '' => 'D',
-	                 '111101000011100100001000011110' => 'E',
-	                 '111101000011100100001000010000' => 'F',
-	                 '011001001010000101101001001110' => 'G',
-	                 '100101001011110100101001010010' => 'H',
-	                 '' => 'I',
-	                 '001100001000010000101001001100' => 'J',
-	                 '100101010011000101001010010010' => 'K',
-	                 '100001000010000100001000011110' => 'L',
-	                 '' => 'M',
-	                 '' => 'N',
-	                 '' => 'O',
-	                 '111001001010010111001000010000' => 'P',
-	                 '' => 'Q',
-	                 '111001001010010111001010010010' => 'R',
-	                 '' => 'S',
-	                 '' => 'T',
-	                 '100101001010010100101001001100' => 'U',
-	                 '' => 'V',
-	                 '' => 'W',
-	                 '' => 'X',
-	                 '100011000101010001000010000100' => 'Y',
-	                 '111100001000100010001000011110' => 'Z',
-	                ];
-
-	function decodeText($painted) {
-		global $encodedChars;
-
-		[$minX, $minY, $maxX, $maxY] = getBoundingBox($painted);
-		$map = [];
-		foreach (yieldXY($minX, $minY, $maxX, $maxY, true) as $x => $y) {
-			$map[$y][$x] = '0';
-			if (isset($painted[$y][$x])) {
-				$map[$y][$x] = $painted[$y][$x] == '#' ? '1' : '0';
-			}
-		}
-
-		$text = '';
-		$charCount = floor(count($map[0]) / 5);
-		$chars = [];
-
-		foreach ($map as $row) {
-			array_shift($row);
-			for ($i = 0; $i < $charCount; $i++) {
-				$chars[$i][] = implode('', array_slice($row, ($i * 5), 5));
-			}
-		}
-
-		foreach ($chars as $c) {
-			$id = implode('', $c);
-			if (isDebug() && !isset($encodedChars[$id])) { echo 'Unknown Letter: ', $id, "\n"; }
-			$text .= isset($encodedChars[$id]) ? $encodedChars[$id] : '?';
-		}
-
-		return $text;
+	function drawMap($painted) {
+		$map = flattenMap($painted);
+		foreach ($map as $row) { echo implode('', $row), "\n"; }
 	}
 
 	$paintResult = paintHull($input)[0];
 
 	$part1 = 0;
-	foreach ($paintResult as $y => $row) {
-		$part1 += count($row);
-	}
+	foreach ($paintResult as $y => $row) { $part1 += count($row); }
 	echo 'Part 1: ', $part1, "\n";
 
 	$paintResult = paintHull($input, '#')[0];
-	echo 'Part 2: ', decodeText($paintResult), "\n";
+	echo 'Part 2: ', decodeText(flattenMap($paintResult, '1', '0', 0, 1)), "\n";
 	drawMap($paintResult);
--- 8/run.php
+++ 8/run.php
@@ -1,6 +1,7 @@
 #!/usr/bin/php
 <?php
 	require_once(dirname(__FILE__) . '/../common/common.php');
+	require_once(dirname(__FILE__) . '/../common/decodeText.php');
 	$input = getInputLine();
 
 	$width = isTest() ? 2 : 25;
@@ -67,56 +68,6 @@
 		$finalImage = str_replace('1', '█', $finalImage);
 
 		return [$finalImage, $text];
-	}
-
-	$encodedChars = ['011001001010010111101001010010' => 'A',
-	                 '111001001011100100101001011100' => 'B',
-	                 '011001001010000100001001001100' => 'C',
-	                 '' => 'D',
-	                 '111101000011100100001000011110' => 'E',
-	                 '111101000011100100001000010000' => 'F',
-	                 '011001001010000101101001001110' => 'G',
-	                 '100101001011110100101001010010' => 'H',
-	                 '' => 'I',
-	                 '001100001000010000101001001100' => 'J',
-	                 '100101010011000101001010010010' => 'K',
-	                 '100001000010000100001000011110' => 'L',
-	                 '' => 'M',
-	                 '' => 'N',
-	                 '' => 'O',
-	                 '111001001010010111001000010000' => 'P',
-	                 '' => 'Q',
-	                 '111001001010010111001010010010' => 'R',
-	                 '' => 'S',
-	                 '' => 'T',
-	                 '100101001010010100101001001100' => 'U',
-	                 '' => 'V',
-	                 '' => 'W',
-	                 '' => 'X',
-	                 '100011000101010001000010000100' => 'Y',
-	                 '111100001000100010001000011110' => 'Z',
-	                ];
-
-	function decodeText($layers) {
-		global $encodedChars;
-
-		$text = '';
-		$charCount = strlen($layers[0]) / 5;
-		$chars = [];
-
-		foreach ($layers as $layer) {
-			for ($i = 0; $i < $charCount; $i++) {
-				$chars[$i][] = substr($layer, ($i * 5), 5);
-			}
-		}
-
-		foreach ($chars as $c) {
-			$id = implode('', $c);
-			if (isDebug() && !isset($encodedChars[$id])) { echo 'Unknown Letter: ', $id, "\n"; }
-			$text .= isset($encodedChars[$id]) ? $encodedChars[$id] : '?';
-		}
-
-		return $text;
 	}
 
 	$layers = getLayers($input, $width, $height);
