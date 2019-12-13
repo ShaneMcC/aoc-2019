45e67d8c1645d6d5ab2a7b8c66f2910281da3a01
Shane Mc Cormack <shanemcc@gmail.com>
2019-12-13 00:44:44 +0000

    Day 12. Part 1. 9369. Late.

---
+++ 12/run.php
@@ -0,0 +1,67 @@
+#!/usr/bin/php
+<?php
+	require_once(dirname(__FILE__) . '/../common/common.php');
+	$input = getInputLines();
+
+	$moons = [];
+	foreach ($input as $line) {
+		preg_match('#<x=([-0-9]+), y=([-0-9]+), z=([-0-9]+)>#i', $line, $m);
+		[$all, $x, $y, $z] = $m;
+		$moons[] = ['pos' => ['x' => (int)$x, 'y' => (int)$y, 'z' => (int)$z], 'vel' => ['x' => 0, 'y' => 0, 'z' => 0]];
+	}
+
+	function simulate($moons) {
+		// Calculate Velocity
+		for ($a = 0; $a < count($moons); $a++) {
+			for ($b = $a + 1; $b < count($moons); $b++) {
+				foreach (['x', 'y', 'z'] as $c) {
+					if ($moons[$a]['pos'][$c] > $moons[$b]['pos'][$c]) {
+						$moons[$a]['vel'][$c]--;
+						$moons[$b]['vel'][$c]++;
+					} else if ($moons[$a]['pos'][$c] < $moons[$b]['pos'][$c]) {
+						$moons[$a]['vel'][$c]++;
+						$moons[$b]['vel'][$c]--;
+					}
+				}
+			}
+		}
+
+		// Move based on velocity
+		for ($i = 0; $i < count($moons); $i++) {
+			foreach (['x', 'y', 'z'] as $c) {
+				$moons[$i]['pos'][$c] += $moons[$i]['vel'][$c];
+			}
+		}
+
+		return $moons;
+	}
+
+	function dumpMoons($moons) {
+		foreach ($moons as $moon) {
+			echo sprintf('pos=<x=%-4d, y=%-4d, z=%-4d>, vel=<x=%-4d, y=%-4d, z=%-4d>', $moon['pos']['x'], $moon['pos']['y'], $moon['pos']['z'], $moon['vel']['x'], $moon['vel']['y'], $moon['vel']['z']), "\n";
+		}
+		echo 'Energy: ', getEnergy($moons), "\n";
+	}
+
+	function getEnergy($moons) {
+		$energy = 0;
+
+		foreach ($moons as $moon) {
+			$potential = $kinetic = 0;
+			foreach (['x', 'y', 'z'] as $c) {
+				$potential += abs($moon['pos'][$c]);
+				$kinetic += abs($moon['vel'][$c]);
+			}
+			$energy += ($potential * $kinetic);
+		}
+
+		return $energy;
+	}
+
+
+	$part1 = $moons;
+	for ($i = 0; $i < 1000; $i++) {
+		$part1 = simulate($part1);
+	}
+
+	echo 'Part 1: ', getEnergy($part1), "\n";
