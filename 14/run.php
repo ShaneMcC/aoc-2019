3184eae768ddef876e0f2534980242caeebbdf8e
Shane Mc Cormack <shanemcc@gmail.com>
2019-12-14 18:18:37 +0000

    Remove unneeded duplication.

--- 14/run.php
+++ 14/run.php
@@ -24,6 +24,12 @@
 	function requiredOre($reactions, $item, $count = 1, $spare = [], $indent = 0) {
 		$requiredOre = 0;
 
+		if ($item == 'ORE') {
+			debugOut(str_repeat("\t", $indent), 'Using ', $count, ' ore', "\n");
+
+			return [$count, $spare];
+		}
+
 		debugOut(str_repeat("\t", $indent), 'Need: ', $count, ' of ', $item, "\n");
 
 		// Take into account any spares we already have.
@@ -57,57 +63,10 @@
 
 		debugOut(str_repeat("\t", $indent), "\t", 'leaving: ', ($produced - $count), ' (now ', (isset($spare[$item]) ? $spare[$item] : 0), ') spare', "\n");
 
-		// Bits of our requirements that can be produced directly with ore.
+		// Do the child reactions required.
 		foreach ($reactions[$item]['required'] as $ritem => $rcount) {
-			if (isset($reactions[$ritem]['required']['ORE'])) {
-				// How many of this item do we need?
-				$required = $rcount * $multiplier;
-
-				debugOut(str_repeat("\t", $indent), "\t", 'Need: ', $required, ' of ', $ritem, "\n");
-
-				// Take into account any spares we already have.
-				if (isset($spare[$ritem])) {
-					if ($spare[$ritem] >= $required) {
-						$spare[$ritem] -= $required;
-						debugOut(str_repeat("\t", $indent), "\t\t", 'Used: ', $required, ' spares leaving ', $spare[$ritem], "\n");
-						continue;
-					} else {
-						debugOut(str_repeat("\t", $indent), "\t\t", 'Used all ', $spare[$ritem], ' spares leaving 0', "\n");
-						$required -= $spare[$ritem];
-						unset($spare[$ritem]);
-					}
-				}
-
-				debugOut(str_repeat("\t", $indent), "\t\t", 'Still need: ', $required, ' of ', $ritem, "\n");
-
-				// How many reactions to produce the amount required?
-				$rmultiplier = ceil($required / $reactions[$ritem]['count']);
-
-				// How many items are produced total?
-				$rproduced = $rmultiplier * $reactions[$ritem]['count'];
-
-				// How much ore did this use?
-				$requiredOre += ($rmultiplier * $reactions[$ritem]['required']['ORE']);
-
-				debugOut(str_repeat("\t", $indent), "\t\t", $rmultiplier, ' reactions will produce: ', $rproduced, ' using ', ($rmultiplier * $reactions[$ritem]['required']['ORE']), ' ore', "\n");
-
-				// Store spares..
-				if ($rproduced > $required) {
-					if (!isset($spare[$ritem])) { $spare[$ritem] = 0; }
-					$spare[$ritem] += ($rproduced - $required);
-				}
-
-				debugOut(str_repeat("\t", $indent), "\t\t", 'leaving: ', ($rproduced - $required), ' (now ', (isset($spare[$ritem]) ? $spare[$ritem] : 0), ') spare', "\n");
-			}
-		}
-
-		// Now produce other items.
-		// Items that can not be produced directly with ore.
-		foreach ($reactions[$item]['required'] as $ritem => $rcount) {
-			if (!isset($reactions[$ritem]['required']['ORE'])) {
-				[$o, $spare] = requiredOre($reactions, $ritem, ($rcount * $multiplier), $spare, $indent + 1);
-				$requiredOre += $o;
-			}
+			[$o, $spare] = requiredOre($reactions, $ritem, ($rcount * $multiplier), $spare, $indent + 1);
+			$requiredOre += $o;
 		}
 
 		return [(int)$requiredOre, $spare];
