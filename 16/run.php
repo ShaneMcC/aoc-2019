90daef923de0605171b5ef041acb77c2b70c7da3
Shane Mc Cormack <shanemcc@gmail.com>
2019-12-16 16:35:43 +0000

    Day 16. Part 2. 2153. It's slow, I don't understand it, and I hate it. (shrug)

--- 16/run.php
+++ 16/run.php
@@ -1,13 +1,11 @@
 #!/usr/bin/php
 <?php
 	require_once(dirname(__FILE__) . '/../common/common.php');
-	$input = getInputLine();
+	$start = getInputLine();
 
-	$start = str_split($input);
-	$pattern = [0, 1, 0, -1];
-
-	function calculate($input, $pattern) {
+	function calculate($input) {
 		$result = [];
+		$pattern = [0, 1, 0, -1];
 
 		for ($i = 0; $i < count($input); $i++) {
 			$val = 0;
@@ -21,9 +19,40 @@
 		return $result;
 	}
 
+	function doPart1($start) {
+		$input = str_split($start);
+		for ($i = 0; $i < 100; $i++) {
+			$input = calculate($input);
+		}
 
-	$input = $start;
-	for ($i = 0; $i < 100; $i++) {
-		$input = calculate($input, $pattern, true);
+		return implode('', array_slice($input, 0, 8));
 	}
-	echo 'Part 1: ', implode('', array_slice($input, 0, 8)), "\n";
+
+	$part1 = doPart1($start);
+	echo 'Part 1: ', $part1, "\n";
+
+	// Based on: https://www.reddit.com/r/adventofcode/comments/ebai4g/2019_day_16_solutions/
+	//
+	// There is some magic here, fucked if I know what.
+	function doPart2($start) {
+		$input = str_split(str_repeat($start, 10000));
+		$skip = (int)substr($start, 0, 7);
+
+		for ($i = 0; $i < 100; $i++) {
+		    $checksum = array_sum(array_slice($input, $skip));
+
+		    $newInput = array_fill(0, $skip, 0);
+			$newInput[] = (abs($checksum) % 10);
+
+			for ($j = $skip + 1; $j < count($input); $j++) {
+	        	$checksum -= $input[$j - 1];
+	        	$newInput[] = (abs($checksum) % 10);
+	    	}
+    		$input = $newInput;
+    	}
+
+    	return implode('', array_slice($input, $skip, 8));
+    }
+
+	$part2 = doPart2($start);
+	echo 'Part 2: ', $part2, "\n";
