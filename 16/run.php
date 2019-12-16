90299cbd350369348a2a356a6cec393e6571e799
Shane Mc Cormack <shanemcc@gmail.com>
2019-12-16 15:06:46 +0000

    Optimise part 1 a bit.

--- 16/run.php
+++ 16/run.php
@@ -3,22 +3,18 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLine();
 
-	$input = str_split($input);
+	$start = str_split($input);
 	$pattern = [0, 1, 0, -1];
 
 	function calculate($input, $pattern) {
 		$result = [];
 
 		for ($i = 0; $i < count($input); $i++) {
-
-			// TOOD: Maths.
-			$thisPattern = [];
-			foreach ($pattern as $bit) { $thisPattern = array_merge($thisPattern, array_fill(0, $i + 1, $bit)); }
-
 			$val = 0;
-			for ($j = 0; $j < count($input); $j++) {
-				// TODO: Maths.
-				$patternBit = $thisPattern[($j + 1) % count($thisPattern)];
+			for ($j = $i; $j < count($input); $j++) {
+				$patternLen = count($pattern) * ($i + 1);
+				$patternPos = (($j + 1) % $patternLen);
+				$patternBit = $pattern[floor($patternPos / ($patternLen / count($pattern)))];
 				$val += ($input[$j] * $patternBit);
 			}
 			$result[] = (abs($val) % 10);
@@ -27,8 +23,9 @@
 		return $result;
 	}
 
-	for ($i = 0; $i < 100; $i++) {
-		$input = calculate($input, $pattern);
-	}
 
+	$input = $start;
+	for ($i = 0; $i < 100; $i++) {
+		$input = calculate($input, $pattern, true);
+	}
 	echo 'Part 1: ', implode('', array_slice($input, 0, 8)), "\n";
