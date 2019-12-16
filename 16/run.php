d531ddf41ac6fa9b08d396f508d85178fb598698
Shane Mc Cormack <shanemcc@gmail.com>
2019-12-16 16:47:39 +0000

    I understand it now.

--- 16/run.php
+++ 16/run.php
@@ -34,6 +34,23 @@
 	// Based on: https://www.reddit.com/r/adventofcode/comments/ebai4g/2019_day_16_solutions/
 	//
 	// There is some magic here, fucked if I know what.
+	//
+	// Something like:
+	//  - Because "the inputs in the pattern are repeated <digit number> of times"
+	//    everything before the current <digit number> will always be 0, so we
+	//    only need to worry about digits after us.
+	//
+	//  - Because we are offset into the latter half of the puzzle, we end up
+	//    only actually ever having [0, 0, 0, 0, ... 1, 1, ... 1, 1] for the
+	//    pattern, so our checksum becomes a sum() operation on the remaining
+	//    digits after us.
+	//
+	//  - Once we've calculated the checksum once, we can then skip through the
+	//    rest and subtract the previous digit from the value (because it would
+	//    be patterned to 0 this time).
+	//
+	//  - Get number.
+	//
 	function doPart2($start) {
 		$input = str_split(str_repeat($start, 10000));
 		$skip = (int)substr($start, 0, 7);
