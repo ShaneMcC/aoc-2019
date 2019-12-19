31d483348fa6c4968fbf53a0843f700dc616517d
Shane Mc Cormack <shanemcc@gmail.com>
2019-12-19 12:36:00 +0000

    Day 19. Part 2. 2003. Definitely a better way of doing this.

--- 19/run.php
+++ 19/run.php
@@ -8,9 +8,7 @@
 		$vm = new IntCodeVM(IntCodeVM::parseInstrLines($input));
 		$vm->appendInput($x);
 		$vm->appendInput($y);
-		$vm->run();
-
-		echo '[', $x, ', ', $y, ']', "\n";
+		while (count($vm->getAllOutput()) == 0) { $vm->step(); }
 
 		return $vm->getAllOutput()[0];
 	}
@@ -32,8 +30,50 @@
 
 		$out = testXY($input, $x, $y);
 		$part1 += $out;
-		$map[$y][$x] = $out;
+		$map[$y][$x] = $out ? '#' : ' ';
 	}
 
 	drawMap($map);
 	echo 'Part 1: ', $part1, "\n";
+
+	$map = [];
+	$y = 1500; // Reasonable starting guess?
+	$found = false;
+	$part2 = [0, 0];
+	while (true) {
+		$y++;
+
+		$foundFirst = false;
+		$startX = 0;
+
+		// Look for a line at least 100 wide
+		for ($x = $y;; $x++) {
+			$out = testXY($input, $x, $y);
+
+			if ($out == 1) {
+				$startX = $x;
+
+				$out2 = testXY($input, $x + 99, $y);
+				if ($out2 != '1') {
+					continue 2;
+				} else {
+					break;
+				}
+			}
+		}
+		// Look across the line to see if we get a valid 100x100 box.
+		for ($x2 = $startX + 90 ;; $x2++) {
+			$corners = testXY($input, $x2, $y);
+			if ($corners == 0) { continue 2; }
+
+			$corners += testXY($input, $x2 + 99, $y);
+			$corners += testXY($input, $x2, $y + 99);
+			$corners += testXY($input, $x2 + 99, $y + 99);
+			if ($corners == 4) { $part2 = [$x2, $y]; break; }
+		}
+
+		// We have a box.
+		break;
+	}
+
+	echo 'Part 2: ', ($part2[0] * 10000) + $part2[1], "\n";
