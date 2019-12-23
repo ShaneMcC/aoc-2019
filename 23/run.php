c17c1f4e0272ddc5200782bde64c78c402fb0b1c
Shane Mc Cormack <shanemcc@gmail.com>
2019-12-23 05:18:56 +0000

    Day 23. Part 1. 23. Should have been quicker.

--- 23/run.php
+++ 23/run.php
@@ -11,8 +11,13 @@
 		$computers[] = $vm;
 	}
 
+	$nat = null;
+	$part1 = false;
+	$lastNatY = null;
+
+	$empty = [];
 	while (true) {
-		foreach ($computers as $c) {
+		foreach ($computers as $cid => $c) {
 			try {
 				$c->step();
 
@@ -22,16 +27,35 @@
 					$y = $c->getOutput();
 
 					if ($addr == 255) {
-						echo 'Part 1: ', $y, "\n";
-						break 2;
-					}
+						if ($part1 == false) {
+							echo 'Part 1: ', $y, "\n";
+							$part1 = true;
+						}
 
-					$computers[$addr]->appendInput($x);
-					$computers[$addr]->appendInput($y);
+						$nat = [$x, $y];
+					} else {
+						unset($empty[$addr]);
+						$computers[$addr]->appendInput($x);
+						$computers[$addr]->appendInput($y);
+					}
 				}
 
 			} catch (Exception $e) {
+				$empty[$cid] = true;
 				$c->appendInput(-1);
 			}
+		}
+
+		if (count($empty) == 50 && $nat != null) {
+			$computers[0]->appendInput($nat[0]);
+			$computers[0]->appendInput($nat[1]);
+
+			if ($lastNatY == $nat[1]) {
+				echo 'Part 2: ', $lastNatY, "\n";
+				break;
+			}
+
+			$lastNatY = $nat[1];
+			$nat = null;
 		}
 	}
